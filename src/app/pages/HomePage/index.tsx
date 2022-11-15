66bf85bd252ed8737c69177991c079fcbae0dc1e
007vict <super_vg@bk.ru>
2022-11-15 21:24:15 +0300

    generate Benefits

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -3,12 +3,14 @@ import * as React from 'react';
 import { Header } from 'app/components/Header/index';
 import { Footer } from 'app/components//Footer';
 import { Promo } from 'app/components/Promo';
+import { Benefits } from 'app/components/Benefits';
 
 export function HomePage() {
   return (
     <>
       <Header/>
       <Promo />
+      <Benefits />
       <Footer/>
     </>
   );
